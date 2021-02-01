2ecee4ab5ca19ef38eee0407ba3841612b86b077
James Sapp <jameseisapp@gmail.com>
2021-02-01 17:09:49 -0600

    commit tree 42ab7979cc55a1794cc92048d15f65735cfe1370

--- index.js
+++ index.js
@@ -10,4 +10,30 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+// function greet(time) {
+//   if (time < "12:00") {
+//     return "Good Morning"
+//   } else if (time > "17:00") {
+//     return "Good Evening"
+//   } else {
+//     return "Good Afternoon"
+//   }
+// }
+
+function greet(time) {
+  const hour = parseInt(time, 10);
+  if (hour < 12) {
+    return "Good Morning"
+  } else if (hour > 17) {
+    return "Good Evening"
+  } else {
+    return "Good Afternoon"
+  }
+}
+
+function displayMessage(message) {
+  const input = document.getElementById('greeting');
+  input.innerText = message
+}
 /* Write your implementation of displayMessage() */
